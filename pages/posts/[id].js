7a3307649b1421c5b6e59925dc73a5fec26a892b
liamzebedee <liamzebedee@yahoo.com.au>
2022-12-23 17:03:15 +1100

    .

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -39,7 +39,7 @@ function Post() {
                 <ReactMarkdown 
                     components={{
                         // open new tab <a href="" target="_blank">
-                        img: ({ node, ...props }) => <a href={props.src} rel="noreferrer" target="_blank"><Image {...props} alt="" style={{ maxWidth: '100%' }} /></a>
+                        img: ({ node, ...props }) => <a href={props.src} rel="noreferrer" target="_blank"><img {...props} alt="" style={{ maxWidth: '100%' }} /></a>
                     }}>
                     {smartquotes.string(fileContent.content)}
                 </ReactMarkdown>
